57ac6101bbfb670fe4c76bc075a90c47311ac2f4
comefan <364988317@qq.com>
2020-04-14 14:38:44 +0800

    增加delete弹出框component

---
+++ ionic/myApp/src/app/common/open-modal/open-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-open-modal',
+  templateUrl: './open-modal.component.html',
+  styleUrls: ['./open-modal.component.scss'],
+})
+export class OpenModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
